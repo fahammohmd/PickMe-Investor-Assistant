aeb5972f37d0abef23a0a609028ad8734e5e6a7d
faham <faham084@gmail.com>
2025-12-17 09:12:07 +0530

    Feat: Update FCFF values and use mid-year discounting

--- src/valuation_logic.py
+++ src/valuation_logic.py
@@ -6,10 +6,10 @@ import numpy as np
 # Hardcoded default assumptions for the static "Implied Price" calculation
 DEFAULT_FORECAST_ASSUMPTIONS = {
     'fcff_forecast': [
-        541806212.22, 1125496234.03, 2162454601.03, 4391014760.88, 5652970707.16,
-        6575053076.16, 7955001262.83, 9562063302.82, 11418747503.57, 13546453597.64
+        1241793558, 1125496234, 2162454601, 4391014761, 5652970707,
+        6575053076, 7955001263, 9562063303, 11418747504, 13546453598
     ],
-    'net_debt': 2125719896,
+    'net_debt':  914785000,
     'shares_outstanding': 333323673,
 }
 DEFAULT_TERMINAL_ASSUMPTIONS = {'wacc': 0.10, 'terminal_growth_rate': 0.03}
@@ -31,8 +31,8 @@ def perform_dcf_calculation(terminal_assumptions, forecast_assumptions):
         fcf_y10 = df['FCF'].iloc[-1]
         terminal_value = (fcf_y10 * (1 + terminal_assumptions['terminal_growth_rate'])) / (terminal_assumptions['wacc'] - terminal_assumptions['terminal_growth_rate'])
 
-    # --- Present Value Calculations ---
-    df['PV Factor'] = [(1 / (1 + terminal_assumptions['wacc'])) ** year for year in years]
+    # --- Present Value of FCF ---
+    df['PV Factor'] = [(1 / (1 + terminal_assumptions['wacc'])) ** (year - 0.5) for year in years]
     df['PV of FCF'] = df['FCF'] * df['PV Factor']
     
     pv_terminal_value = terminal_value * df['PV Factor'].iloc[-1]
